68332959b866f0ea9188ee218891183ff0a8a97e
Subhangi Sati <113504623+SubhangiSati@users.noreply.github.com>
2022-11-24 00:36:53 +0530

    Add files via upload
    lab 5

---
+++ exercise.java
@@ -0,0 +1,7 @@
+import firstpack.sample ;
+public class exercise {
+public static void main(String[]args) {
+	sample a=new sample();
+	a.display();
+}
+}
---
+++ info.java
@@ -0,0 +1,9 @@
+import Balance.Account;
+public class info {
+public static void main(String[]args) {
+	int s=100000;
+	int r= 500000;
+	Account x= new Account();
+	x.Account_display(s+r);
+}
+}
